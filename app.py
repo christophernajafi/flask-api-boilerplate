8d7f38dd05f63e02526b47588e77f82af391f752
Christopher Najafi <chris.najafi@gmail.com>
2020-03-20 14:42:45 -0400

    added route for home directory, will return template with instructions instead of 404

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from flask_jwt import JWT
 
@@ -20,6 +20,12 @@ app.secret_key = 'fidelio'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
